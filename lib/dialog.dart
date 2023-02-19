46fc9c9ff7bc0695a5b5415328744cc1833669a2
Kurogoma939 <kan.sss.939@gmail.com>
2023-02-19 22:52:37 +0900

    ボタンの実装

---
+++ lib/dialog.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/// 背景透過させるダイアログ
+///
+/// [title] タイトル
+///
+/// [formContent] フォームの内容
+///
+/// [label] ボタンのラベル
+///
+/// [onTap] ボタンを押した時の処理
+class BlurAlertDialog extends StatelessWidget {
+  const BlurAlertDialog({
+    super.key,
+    required this.title,
+    required this.formContent,
+    required this.label,
+    required this.onTap,
+  });
+
+  final String title;
+  final String formContent;
+  final String label;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+      child: AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        title: Text(title),
+        content: TextField(
+          keyboardType: TextInputType.text,
+          maxLines: 1,
+          maxLength: 40,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: formContent,
+            labelStyle: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'キャンセル',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              onTap;
+            },
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage_multiplex/firebase_options.dart';
 import 'package:firebase_storage_multiplex/model/folder_model.dart';
 import 'package:firebase_storage_multiplex/notifier/firebase_storage_notifier.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'dialog.dart';
+import 'folder_detail_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -80,219 +82,166 @@ class _HomePageState extends ConsumerState<HomePage> with RouteAware {
     final state = ref.watch(firebaseStorageNotifierProvider);
 
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 30, 30, 30),
-        appBar: AppBar(
-          title: const Text(
-            'Firestore Multiplex',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 24,
-            ),
+      backgroundColor: const Color.fromARGB(255, 30, 30, 30),
+      appBar: AppBar(
+        title: const Text(
+          'Firestore Multiplex',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 24,
           ),
         ),
-        body: state.fileList.when(
-          data: (data) {
-            return ListView.builder(
-              itemCount: data.length,
-              padding: const EdgeInsets.only(top: 8),
-              itemBuilder: (context, index) {
-                if (data.isEmpty) {
-                  return const Center(
-                    child: Text('No Data'),
-                  );
-                }
-                final item = data[index];
-                return item.folderType == FolderType.folder
-                    ? Card(
-                        child: ListTile(
-                          leading: const Icon(Icons.folder),
+      ),
+      body: Stack(
+        children: [
+          state.fileList.when(
+            data: (data) {
+              return ListView.builder(
+                itemCount: data.length,
+                padding: const EdgeInsets.only(top: 8),
+                itemBuilder: (context, index) {
+                  if (data.isEmpty) {
+                    return const Center(
+                      child: Text('No Data'),
+                    );
+                  }
+                  final item = data[index];
+                  return item.folderType == FolderType.folder
+                      ? Card(
+                          child: ListTile(
+                            leading: const Icon(Icons.folder),
+                            title: Text(
+                              item.folderName,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => FolderDetailPage(
+                                  folderPath: item.folderPath,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      : ListTile(
+                          leading: const Icon(
+                            Icons.file_copy,
+                            color: Colors.white,
+                          ),
                           title: Text(
                             item.folderName,
                             style: const TextStyle(
+                              color: Colors.white,
                               fontSize: 16,
                               fontWeight: FontWeight.w600,
                             ),
                           ),
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => FolderDetailPage(
-                                folderPath: item.folderPath,
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    : ListTile(
-                        leading: const Icon(
-                          Icons.file_copy,
-                          color: Colors.white,
-                        ),
-                        title: Text(
-                          item.folderName,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      );
+                        );
+                },
+              );
+            },
+            loading: () {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+            error: (error, stack) {
+              return const Center(
+                child: Text('error'),
+              );
+            },
+          ),
+          Positioned(
+            bottom: 28,
+            right: 16,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const FolderUploadButton(),
+                const SizedBox(height: 12),
+                MenuButton(
+                  icon: Icons.file_upload_rounded,
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FolderUploadButton extends ConsumerWidget {
+  const FolderUploadButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MenuButton(
+      icon: Icons.folder_open_rounded,
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            // 背景が透ける角丸ダイアログ
+            return BlurAlertDialog(
+              title: 'フォルダを作成',
+              formContent: 'フォルダ名を入力してください',
+              label: '作成',
+              onTap: () async {
+                await ref
+                    .read(firebaseStorageNotifierProvider.notifier)
+                    .createFolder(
+                      currentPath: 'users/',
+                      folderName: 'test',
+                    );
               },
             );
           },
-          loading: () {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-          error: (error, stack) {
-            return const Center(
-              child: Text('error'),
-            );
-          },
-        ));
+        );
+      },
+    );
   }
 }
 
-///
-/// 下の階層のフォルダーを表示するページ
-///
-/// [folderPath] フォルダーのパス
-///
-class FolderDetailPage extends ConsumerStatefulWidget {
-  const FolderDetailPage({
+class MenuButton extends StatelessWidget {
+  const MenuButton({
     super.key,
-    required this.folderPath,
+    required this.icon,
+    required this.onTap,
   });
 
-  final String folderPath;
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _FolderDetailPageState();
-}
-
-class _FolderDetailPageState extends ConsumerState<FolderDetailPage>
-    with RouteAware {
-  @override
-  void initState() {
-    // Unhandled Exception: Tried to modify a provider while the widget tree was building.
-    // 上記エラーを回避するためにWidgetsBinding.instance.addPostFrameCallbackを使用
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      routeObserver.subscribe(this, ModalRoute.of(context)!);
-      Future(() async {
-        if (kDebugMode) {
-          print('folderPath: ${widget.folderPath}');
-        }
-        await ref.read(firebaseStorageNotifierProvider.notifier).getFolderList(
-              folderPath: widget.folderPath,
-            );
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    routeObserver.unsubscribe(this);
-    super.dispose();
-  }
-
-  @override
-  void didPopNext() {
-    Future(() async {
-      if (kDebugMode) {
-        print('folderPath: ${widget.folderPath}');
-      }
-      await ref.read(firebaseStorageNotifierProvider.notifier).getFolderList(
-            folderPath: widget.folderPath,
-          );
-    });
-  }
+  final IconData icon;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(firebaseStorageNotifierProvider);
-
-    return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 30, 30, 30),
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-            ),
-          ),
-          title: const Text(
-            'Firestore Multiplex',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 24,
-            ),
+    return InkWell(
+      borderRadius: BorderRadius.circular(34),
+      onTap: onTap,
+      child: Container(
+        width: 68,
+        height: 68,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(34),
+          color: const Color.fromARGB(94, 9, 74, 186),
+          border: Border.all(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            width: 3,
           ),
         ),
-        body: state.fileList.when(
-          data: (data) {
-            if (data.isEmpty) {
-              return const Center(
-                child: Text('No Data'),
-              );
-            }
-            return ListView.builder(
-              itemCount: data.length,
-              padding: const EdgeInsets.only(top: 8),
-              itemBuilder: (context, index) {
-                final item = data[index];
-                return item.folderType == FolderType.folder
-                    ? Card(
-                        child: ListTile(
-                          leading: const Icon(Icons.folder),
-                          title: Text(
-                            item.folderName,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => FolderDetailPage(
-                                folderPath: item.folderPath,
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    : ListTile(
-                        leading: const Icon(
-                          Icons.file_copy,
-                          color: Colors.white,
-                        ),
-                        title: Text(
-                          item.folderName,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      );
-              },
-            );
-          },
-          loading: () {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-          error: (error, stack) {
-            return const Center(
-              child: Text('error'),
-            );
-          },
-        ));
+        child: Icon(
+          icon,
+          size: 32,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
